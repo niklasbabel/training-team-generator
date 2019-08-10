f2c4efce42fe40e6db67bf966a1ce2cbe5a2c371
Niklas Babel <niklas@wane.co>
2019-08-10 03:35:21 +0200

    scroll modal fix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let inputSize = "";
 let Players = [];
+
+//List Of Persons Images
 let images = [
   "images/anna.png",
   "images/nikolina.png",
@@ -60,6 +62,8 @@ function submitSize() {
   }
 }
 
+
+
 var inputKey = document.getElementById("question2");
 console.log(inputKey);
 inputKey.addEventListener("keydown", function(event) {
@@ -69,6 +73,8 @@ inputKey.addEventListener("keydown", function(event) {
   }
 });
 
+
+//Function For Getting Selected Persons and converting into team
 function submitNames() {
   let selectedSelectors = document
     .getElementById("images-wrapper-selected")
@@ -113,18 +119,27 @@ function submitNames() {
       p.textContent = name;
 
       // returns a random integer from 1 to 9
-      let random = Math.floor(Math.random() * 8) + 1;
-      let role = "";
-      console.log(random);
-      if (random > 0 && random <= 3) {
-        role = "Observer";
-      } else if (random > 3 && random <= 6) {
-        role = "Customer";
-      } else {
-        role = "Employee";
+      let role = "Employee";
+      // let random = Math.floor(Math.random() * 8) + 1;
+      
+      // console.log(random);
+      // if (random > 0 && random <= 3) {
+      //   role = "Observer";
+      // } else if (random > 3 && random <= 6) {
+      //   role = "Customer";
+      // } else {
+      //   role = "Employee";
+      // }
+
+      //First Person is always Observer
+      if(j==0){
+        role = "Observer"
       }
 
-      console.log(random, " ", role);
+      //Second is always customer
+      else if(j==1){
+        role = "Customer"
+      }
 
       let roleParagraph = document.createElement("p");
       roleParagraph.textContent = role;
@@ -139,6 +154,7 @@ function submitNames() {
   }
 }
 
+//Function to generate a team it is a subfunction called in bigger function
 let generateTeamsArray = selected => {
   selected = [...selected];
   let teams = [];
@@ -149,12 +165,15 @@ let generateTeamsArray = selected => {
   return teams;
 };
 
+//Populating The persons in a modal
+
 for (let i = 0; i < images.length; i++) {
   // let playerNameFromSrc = ""
   let div = document.createElement("div");
   div.className = "member available";
   let playersImage = document.createElement("img");
   playersImage.src = images[i];
+  //generating naem from image
   let name = images[i]
     .split("/")
     .pop()
@@ -170,10 +189,12 @@ for (let i = 0; i < images.length; i++) {
 
   div.onclick = function(e) {
     if (div.className.indexOf("available") != -1) {
+       //If it is  unselected , select the person
       div.className = div.className.replace("available", "selected");
       document.getElementById("images-wrapper").removeChild(div);
       document.getElementById("images-wrapper-selected").append(div);
     } else {
+      //If it is  selected , unselect the person  
       div.className = div.className.replace("selected", "available");
       document.getElementById("images-wrapper-selected").removeChild(div);
       document.getElementById("images-wrapper").append(div);
@@ -214,6 +235,47 @@ document
 //   document.getElementById("team-choice").reset();
 // }
 
-function clrPlayers(elementID) {
-  document.getElementById(elementID).innerHTML = "";
+// function clrPlayers(elementID) {
+//   document.getElementById(elementID).innerHTML = "";
+// }
+
+
+//Clear All The Forms And The Teams Generated
+function clrPlayers() {
+  document.getElementById("showSelectedNames").innerHTML = "";
+  document.getElementById("input-size").value = "";
+  document.getElementById("images-wrapper").innerHTML = "";
+  document.getElementById("images-wrapper-selected").innerHTML = "";
+  for (let i = 0; i < images.length; i++) {
+    // let playerNameFromSrc = ""
+    let div = document.createElement("div");
+    div.className = "member available";
+    let playersImage = document.createElement("img");
+    playersImage.src = images[i];
+    let name = images[i]
+      .split("/")
+      .pop()
+      .split(".")
+      .slice(0, -1)
+      .join(".");
+    playersImage.className = "player-image";
+    let p = document.createElement("p");
+    name = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
+    p.textContent = name;
+    div.append(playersImage);
+    div.append(p);
+  
+    div.onclick = function(e) {
+      if (div.className.indexOf("available") != -1) {
+        div.className = div.className.replace("available", "selected");
+        document.getElementById("images-wrapper").removeChild(div);
+        document.getElementById("images-wrapper-selected").append(div);
+      } else {
+        div.className = div.className.replace("selected", "available");
+        document.getElementById("images-wrapper-selected").removeChild(div);
+        document.getElementById("images-wrapper").append(div);
+      }
+    };
+    document.getElementById("images-wrapper").append(div);
+  }
 }
